7414c247ded2509c5ae53718e013004dfca50683
vishanth.vishwanath <vishanth.vishwanath@7edge.com>
2025-06-07 12:49:17 +0530

    inital project setup

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from 'react-native-config';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{`Open up App.js to start working on your app! ${Config.env}`}</Text>
       <StatusBar style="auto" />
     </View>
   );
